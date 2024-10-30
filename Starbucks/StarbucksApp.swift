086101c5c6af55d4d61b9d69142a6cb60cb699fe
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2024-10-30 21:35:02 +0300

    Initial Commit

---
+++ Starbucks/StarbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksApp.swift
+//  Starbucks
+//
+//  Created by Hüseyin Umut Kardaş on 30.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
